49af556ba8a21793f9bacb10b640919292084973
George-Okumu <gokumu12@gmail.com>
2022-02-21 06:44:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    cats.slice(-1);
+}
+
+function removeFirstCat() {
+    cats.slice(1);
+}
